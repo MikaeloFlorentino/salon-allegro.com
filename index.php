7cf77c36f6af46552049fa2ee5049122afd0a2b8
Mikaelo Florentino <miguel.hdz.mrn@gmail.com>
2020-01-25 11:53:17 -0600

    agregar fotos

--- index.php
+++ index.php
@@ -183,30 +183,28 @@ die("Revelando secretos");
 				</div>
 			</article>
 
-		<!-- Portfolio -->
-		<!-- 
 			<article class="container box style2">
 				<header>
-					<h2>Magnis parturient</h2>
-					<p>Justo phasellus et aenean dignissim<br />
-					placerat cubilia purus lectus.</p>
+					<h2>Hemos sido testigos de <br />Magnos Eventos</h2>
+					<p>Para esas ocaciones especiales<br />
+					para festejar ese bello momento.</p>
 				</header>
 				<div class="inner gallery">
 					<div class="row gtr-0">
-						<div class="col-3 col-12-mobile"><a href="images/fulls/01.jpg" class="image fit"><img src="images/thumbs/01.jpg" alt="" title="Ad infinitum" /></a></div>
-						<div class="col-3 col-12-mobile"><a href="images/fulls/02.jpg" class="image fit"><img src="images/thumbs/02.jpg" alt="" title="Dressed in Clarity" /></a></div>
-						<div class="col-3 col-12-mobile"><a href="images/fulls/03.jpg" class="image fit"><img src="images/thumbs/03.jpg" alt="" title="Raven" /></a></div>
-						<div class="col-3 col-12-mobile"><a href="images/fulls/04.jpg" class="image fit"><img src="images/thumbs/04.jpg" alt="" title="I'll have a cup of Disneyland, please" /></a></div>
+						<div class="col-3 col-12-mobile"><a href="images/fulls/01.jpg" class="image fit"><img src="images/thumbs/01.jpg" alt="" title="Sean bienvenidos" /></a></div>
+						<div class="col-3 col-12-mobile"><a href="images/fulls/02.jpg" class="image fit"><img src="images/thumbs/02.jpg" alt="" title="a Salon Allegro" /></a></div>
+						<div class="col-3 col-12-mobile"><a href="images/fulls/03.jpg" class="image fit"><img src="images/thumbs/03.jpg" alt="" title="Baby Showers" /></a></div>
+						<div class="col-3 col-12-mobile"><a href="images/fulls/04.jpg" class="image fit"><img src="images/thumbs/04.jpg" alt="" title="Bautizos" /></a></div>
 					</div>
 					<div class="row gtr-0">
-						<div class="col-3 col-12-mobile"><a href="images/fulls/05.jpg" class="image fit"><img src="images/thumbs/05.jpg" alt="" title="Cherish" /></a></div>
-						<div class="col-3 col-12-mobile"><a href="images/fulls/06.jpg" class="image fit"><img src="images/thumbs/06.jpg" alt="" title="Different." /></a></div>
-						<div class="col-3 col-12-mobile"><a href="images/fulls/07.jpg" class="image fit"><img src="images/thumbs/07.jpg" alt="" title="History was made here" /></a></div>
-						<div class="col-3 col-12-mobile"><a href="images/fulls/08.jpg" class="image fit"><img src="images/thumbs/08.jpg" alt="" title="People come and go and walk away" /></a></div>
+						<div class="col-3 col-12-mobile"><a href="images/fulls/05.jpg" class="image fit" width="531" heigt="1032px"><img src="images/thumbs/05.jpg" alt="" title="Primera Comunion" /></a></div>
+						<div class="col-3 col-12-mobile"><a href="images/fulls/06.jpg" class="image fit"><img src="images/thumbs/06.jpg" alt="" title="Graduaciones" /></a></div>
+						<div class="col-3 col-12-mobile"><a href="images/fulls/07.jpg" class="image fit"><img src="images/thumbs/07.jpg" alt="" title="Estamos para todos" /></a></div>
+						<div class="col-3 col-12-mobile"><a href="images/fulls/08.jpg" class="image fit"><img src="images/thumbs/08.jpg" alt="" title="Y hasta Bodas" /></a></div>
 					</div>
 				</div>
 			</article>
--->
+
 		<!-- Contact -->
 			<article class="container box style3">
 				<header>
@@ -216,11 +214,13 @@ die("Revelando secretos");
 				<form method="post" action="#">
 					<div class="row gtr-50">
 						<div class="col-6 col-12-mobile"><input type="text" class="text" name="name" placeholder="Nombre" /></div>
-						<div class="col-6 col-12-mobile"><input type="text" class="text" name="email" placeholder="Correo" /></div>
+						<div class="col-6 col-12-mobile"><input type="text" class="text" name="celular" placeholder="celular" /></div>
+						<div class="col-12">
+							<input type="text" class="text" name="email" placeholder="Correo" /></div>
 						<div class="col-12">
 							<textarea name="message" placeholder="Mensaje"></textarea>
 						</div>
-						 <input type="hidden" id="custId" name="custId" value="<?php echo rand(100000, 999999); ?>">
+						 <input type="hidden" id="custId" name="custId" value="12">
                         <center><div class="g-recaptcha" data-sitekey="6Lf91tAUAAAAAF1qWaRxRc8GYto0bsnDYwdIufYn"></div></center>
 						<div class="col-12">
 							<ul class="actions">
